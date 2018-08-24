0f5594934eac44ba42ed3f16b235f147eed38b08
JESSIE XU (ITA-ISDC-ISD-OOCLL/ZHA) <XUJE8@OOCL.COM>
2018-08-24 14:08:43 +0800

    [jessie] add a unit test when customer has no rental

---
+++ src/test/java/rentalstore/CustomerTest.java
@@ -0,0 +1,18 @@
+package rentalstore;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CustomerTest {
+    private Customer customer = new Customer("Jessie");
+
+    @Test
+    public void should_return_correct_statement_given_customer_has_no_rental() {
+        String statement = customer.statement();
+        assertEquals("Rental Record for Jessie\nAmount owed is 0.0\nYou earned 0 frequent renter points", statement);
+    }
+
+
+
+}
