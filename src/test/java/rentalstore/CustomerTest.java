7a310599c13f28b235eababdf4c4616fdc8b07bc
JESSIE XU (ITA-ISDC-ISD-OOCLL/ZHA) <XUJE8@OOCL.COM>
2018-08-24 14:16:56 +0800

    [jessie] add a unit test when customer rent a regular movie for three day

--- src/test/java/rentalstore/CustomerTest.java
+++ src/test/java/rentalstore/CustomerTest.java
@@ -15,16 +15,29 @@ public class CustomerTest {
 
     @Test
     public void should_return_correct_statement_given_customer_has_rent_one_regular_movie_for_1_day() {
-        Movie movie = new Movie("Titanic", 0);
-        Rental oneDayRental = new Rental(movie, 1);
+        Movie regularMovie = new Movie("Titanic", 0);
+        Rental oneDayRental = new Rental(regularMovie, 1);
         customer.addRental(oneDayRental);
 
         String statement = customer.statement();
 
         assertEquals("Rental Record for Jessie\n" +
-                "\t" + movie.getTitle() + "\t2.0\n" +
+                "\t" + regularMovie.getTitle() + "\t2.0\n" +
                 "Amount owed is 2.0\n" +
                 "You earned 1 frequent renter points", statement);
     }
 
+    @Test
+    public void should_return_correct_statement_given_customer_has_rent_one_regular_movie_for_3_day() {
+        Movie regularMovie = new Movie("Titanic", 0);
+        Rental threeDayRental = new Rental(regularMovie, 3);
+        customer.addRental(threeDayRental);
+
+        String statement = customer.statement();
+
+        assertEquals("Rental Record for Jessie\n" +
+                "\t" + regularMovie.getTitle() + "\t3.5\n" +
+                "Amount owed is 3.5\n" +
+                "You earned 1 frequent renter points", statement);
+    }
 }
